992b42788caaaadd23652d7e62234bb3007276c9
anjali22 <anjaliprajapati3122@gmail.com>
2017-09-28 18:15:21 +0530

    splash screen and app icon

--- app/src/main/java/com/example/aniruddh/eatburp1/Main2Activity.java
+++ app/src/main/java/com/example/aniruddh/eatburp1/Main2Activity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
-    TextView tv;
+
     ImageView iv;
 
     @Override
@@ -24,14 +24,14 @@ public class Main2Activity extends AppCompatActivity {
 
 
 
-        tv=(TextView) findViewById(R.id.textView);
+
         iv=(ImageView) findViewById(R.id.imageView);
 
         Animation myanim= AnimationUtils.loadAnimation(this,R.anim.mytransition);
-        tv.startAnimation(myanim);
+
         iv.startAnimation(myanim);
 
-        tv.animate().setDuration(2000);
+
 
         final Intent i= new Intent(this, MainActivity.class);
 
