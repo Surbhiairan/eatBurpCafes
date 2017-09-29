6a534c14ace56db320474910dbd518e0c6529582
anjali22 <anjaliprajapati3122@gmail.com>
2017-09-29 20:20:40 +0530

    cafe layout

--- app/src/main/java/com/example/aniruddh/eatburp1/CafeOne.java
+++ app/src/main/java/com/example/aniruddh/eatburp1/CafeOne.java
@@ -1,11 +1,15 @@
 package com.example.aniruddh.eatburp1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class CafeOne extends AppCompatActivity {
 
@@ -15,13 +19,50 @@ public class CafeOne extends AppCompatActivity {
         setContentView(R.layout.activity_cafe_one);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);  //to show back button on toolbar
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        toolbar.setTitle("");
+
+
+        Button bookMealButton = (Button)findViewById(R.id.bookMealButton);
+        // Capture button clicks
+        bookMealButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(CafeOne.this,
+                        BookMeal.class);
+                startActivity(myIntent);
+            }
+        });
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });*/
+
+        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.setTitle("Cafe Illiterati");
+                    isShow = true;
+                } else if(isShow) {
+                    collapsingToolbarLayout.setTitle(" ");//carefull there should a space between double quote otherwise it wont work
+                    isShow = false;
+                }
             }
         });
     }
