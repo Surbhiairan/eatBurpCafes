35765ec99ddea56d9619d4e97b948b7befa7fdf8
anjali22 <anjaliprajapati3122@gmail.com>
2017-09-30 12:50:13 +0530

    book meal button

---
+++ app/src/main/java/com/example/aniruddh/eatburp1/Model.java
@@ -0,0 +1,23 @@
+package com.example.aniruddh.eatburp1;
+
+/**
+ * Created by hp on 9/29/2017.
+ */
+
+
+public class Model{
+    String name;
+    int value; /* 0 -&gt; checkbox disable, 1 -&gt; checkbox enable */
+
+    Model(String name, int value){
+        this.name = name;
+        this.value = value;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getValue(){
+        return this.value;
+    }
+
+}
